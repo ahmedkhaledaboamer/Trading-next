feebbea81b4b1431ccb0e73cca2a6055bfb3eb25
Mohamed176b <moshafey18@gmail.com>
2026-02-25 23:05:44 +0400

    Edit business-identity

--- app/[locale]/business-identity/page.tsx
+++ app/[locale]/business-identity/page.tsx
@@ -1,20 +1,21 @@
-import { BrandConclusion } from "@/components/screens/business-path/BrandConclusion";
-import { BrandIntro } from "@/components/screens/business-path/BrandIntro";
-import { BrandOperations } from "@/components/screens/business-path/BrandOperations";
-import { BrandPartner } from "@/components/screens/business-path/BrandPartner";
-import { BrandStatement } from "@/components/screens/business-path/BrandStatement";
-import { HeroSection } from "@/components/screens/business-path/HeroSection";
-import { getTranslations } from "next-intl/server";
+import { BrandConclusion } from "@/components/screens/business-identity/BrandConclusion";
+import { BrandIntro } from "@/components/screens/business-identity/BrandIntro";
+import { BrandOperations } from "@/components/screens/business-identity/BrandOperations";
+import { BrandPartner } from "@/components/screens/business-identity/BrandPartner";
+import { BrandStatement } from "@/components/screens/business-identity/BrandStatement";
+import { HeroSection } from "@/components/screens/business-identity/HeroSection";
+import { getLocale, getTranslations } from "next-intl/server";
 
 export default async function BusinessIdentityPage() {
   const t = await getTranslations("pages.businessIdentity");
+  const locale = await getLocale();
 
   return (
     <>
         
     <HeroSection />
-    <BrandIntro />
-    <BrandStatement />
+    <BrandIntro locale={locale} />
+    <BrandStatement locale={locale} />
     <BrandOperations />
     <BrandPartner />
     <BrandConclusion />
